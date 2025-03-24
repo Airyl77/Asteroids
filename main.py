780cc30dc89fa3c51ba71a940c43fb4071bf7e9d
lbo <e.patic82@gmail.com>
2025-03-24 22:37:53 +0200

    Added Draw Player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -12,12 +13,22 @@ def main():
     my_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return    
-        screen.fill("black") 
-        pygame.display.flip
+        screen.fill("red")
+        player.draw(screen)
+        pygame.display.flip()
+        #mouse_position = pygame.mouse.get_pos()
+        #points = rotate_triangle(screen.get_rect().center, 20, mouse_position)
+    
+        # pygame.Surface.fill(screen, (255,255,255))
+        #pygame.draw.polygon(screen, (0,0,0), points)
+        # pygame.display.update()
         dt = my_clock.tick(60)  / 1000
 
 if __name__ == "__main__":
