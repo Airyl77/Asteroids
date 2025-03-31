3e999d005005c10ae101ed9044fe3fe1ca6fd556
lbo <e.patic82@gmail.com>
2025-03-31 18:26:53 +0300

    Added rate

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
 
         self.rotation = 0
+        self.timer = 0
 
     # in the player class
     def triangle(self):
@@ -22,6 +23,7 @@ class Player(CircleShape):
          pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -33,6 +35,7 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_SPACE]:
             self.shoot(dt)
+            
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -42,5 +45,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self, dt):
+        if self.timer > 0:
+            return
         new_shot = Shot(self.position.x, self.position.y)
         new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = 0.3
