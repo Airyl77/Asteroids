fa6f01f35ca69d4233472341b18726cfe2b2b3ad
lbo <e.patic82@gmail.com>
2025-03-31 18:16:42 +0300

    Added shot2

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def main():
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    Shot.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     
     player = Player(x, y)
     asteroidField_obj = AsteroidField()
